9c72d7a9669babba6a057d61cd99b7fb158abf76
Arda Demir <ardadem.dev@gmail.com>
2018-05-09 20:18:05 +0300

    comments

--- maker.py
+++ maker.py
@@ -4,7 +4,7 @@ import sys
 import json
 import shutil
 import sqlite3
-import hashlib # for future
+import hashlib # for the future
 import argparse
 import requests
 import subprocess
@@ -12,6 +12,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
+# Argument parser stuff
 parser = argparse.ArgumentParser()
 parser.add_argument("device", help="import device json file")
 parser.add_argument("version", help="choose miui version for generate firmware zip")
@@ -20,6 +21,7 @@ parser.add_argument("--skip-miui-release-check", help="skip miui release check",
 parser.parse_args()
 args = parser.parse_args()
 
+# If exists local device.json file, use it. Or fetch from GitHub.
 if os.path.isfile(args.device):
     with open(args.device, 'r') as device_data_file:
         ddata = json.load(device_data_file)
@@ -29,30 +31,38 @@ else:
 
 print("Current device: %s, %s" % (ddata['codename'], ddata['name']))
 
+# Parse miui download page
 page = requests.get("http://en.miui.com/download-" + ddata['id'] + ".html").text
 soup = BeautifulSoup(page, 'html.parser')
 
 for line in soup.find(id=ddata['content_id'][args.version.split('-')[0]]).find_all('a', class_='btn_5'):
+    # Define miui download url
     zip_url = line['href']
+    # Split miui release and miui zip name
     zip_url_split = list(filter(None, urlparse(zip_url).path.split('/')))
+    # Define miui release
     miui_release = zip_url_split[0]
+    # Choose dev or stable link according to option
     if re.match("[0-9].[0-9].[0-9]", miui_release) and "dev" in args.version:
         break
     elif "stable" in args.version:
         break
 
+# If not defined zip url, terminate
 if not 'zip_url' in globals():
     print("Not found any url")
     sys.exit(1)
 
 print("Found download url: %s" % zip_url)
 
+# Create (if not exists) device db & table for caching last release
 cachedb = sqlite3.connect('cache.db')
 cursor = cachedb.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS devices (codename TEXT, version TEXT, last_miui_release REAL)")
 cursor.execute("INSERT INTO devices(codename, version, last_miui_release) SELECT ?, ?, '0.0.0' WHERE NOT EXISTS(SELECT * FROM devices WHERE codename=? and version=?);", [ddata['codename'], args.version, ddata['codename'], args.version])
 cachedb.commit()
 
+# If not defined skip miui release argument, compare last miui release & miui release
 if not args.skip_miui_release_check:
     cursor.execute("SELECT * FROM devices WHERE codename=? and version=?", [ddata['codename'], args.version])
     last_miui_release = cursor.fetchone()[2]
@@ -63,23 +73,28 @@ if not args.skip_miui_release_check:
     
     print("Found new miui build: %s > %s" % (miui_release, last_miui_release))
 
+# If not exists folder, create it
 if not os.path.exists(miui_release):
     os.makedirs(miui_release)
 
 zip_location = miui_release + "/" + zip_url_split[1]
 
+# Fetch miui zip
 if not os.path.isfile(zip_location):
     print("Downloading: %s" % zip_url_split[1])
     with urllib.request.urlopen(zip_url) as response, open(zip_location, 'wb') as outf:
         shutil.copyfileobj(response, outf)
 
+# Create firmware zip
 subprocess.check_call("xiaomi-flashable-firmware-creator/create_flashable_firmware.sh %s %s" % (zip_location, miui_release + "/"), shell=True)
 os.remove(zip_location)
 
 print("Created %s flashable firmware." % ddata['codename'])
 
+# If not defined skip miui release argument, commit last miui version
 if not args.skip_miui_release_check:
     cursor.execute("UPDATE devices SET last_miui_release=? WHERE codename=? and version=?", [miui_release, ddata['codename'], args.version])
     cachedb.commit()
 
+# Finally close cache db
 cachedb.close()
